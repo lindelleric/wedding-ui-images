ded9331ac4f628a4bb4c75cf28502d276731de19
Eric Nilsson <eric.nilsson@hotmail.com>
2019-02-04 18:27:26 +0100

    A word

--- src/components/infoViews/PraktiskInfo/PraktiskInfo.tsx
+++ src/components/infoViews/PraktiskInfo/PraktiskInfo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const PraktiskInfo = () => (
     <div className="info-wrapper">
         <ul className="fancy">
-            <li className="naked">Har ni några frågor eller funderingar så tveka inte på att höra av er;</li>
+            <li className="naked">Har ni frågor eller funderingar så tveka inte på att höra av er:</li>
             <li>Eric Nilsson | 0722-33 35 35 / <a href="mailto:eric.nilsson@hotmail.com">eric.nilsson@hotmail.com</a></li>
             <li>Malin Friman | 0768-60 89 39 / <a href="mailto:malin.friman@hotmail.com">malin.friman@hotmail.com</a></li>
             <li>Anna Sandström | <a href="mailto:anna.sandstrom92@gmail.com">anna.sandstrom92@gmail.com</a></li>
