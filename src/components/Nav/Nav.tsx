f6e0d4f9df8dd5020f4fcffcb048bcdd850ab247
Eric Nilsson <eric.nilsson@hotmail.com>
2019-02-05 20:09:55 +0100

    Add mobile header/navbar

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,91 +2,141 @@ import React from 'react';
 
 import { UISrefActive, UISref } from '@uirouter/react';
 
+import router from './../../router';
+
 // import { Invitation } from './../../types';
 
 import { Current } from './../../generated/graphql';
 
 import './nav.less';
 
-export class Nav extends React.Component<{ invitation: Current.Invitation }> {
-    public isAdmin() {
-        console.log(this.props);
+interface Props {
+    invitation: Current.Invitation;
+}
+
+interface State {
+    isOpen: boolean;
+    currentState: string;
+}
+
+export class Nav extends React.Component<Props, State> {
+
+    public constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false,
+            currentState: this.stateMap[router.stateService.$current.name]
+        };
+
+        router.transitionService.onSuccess({to: '*'}, (trans) => {
+            this.setState({
+                currentState: this.stateMap[router.stateService.$current.name]
+            });
+        });
+    }
+
+    private isAdmin() {
         return this.props.invitation && this.props.invitation.role === 'ADMIN';
     }
 
+    private toggleMenu = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    private stateMap = {
+        rsvp: 'OSA',
+        hittaHit: 'Hitta hit',
+        tidProgram: 'Tid och program',
+        kladkod: 'Klädkod',
+        overnattning: 'Övernattning',
+        onskelista: 'Önskelista',
+        toastmaster: 'Toastmaster',
+        praktiskInfo: 'Kontaktuppgifter',
+        admin: 'Admin'
+    };
+
     public render() {
         return (
-            <div className="nav-wrapper">
-                <ul>
-                    <li>
-                        <UISrefActive class="active">
-                            <UISref to="rsvp">
-                                <a>OSA</a>
-                            </UISref>
-                        </UISrefActive>
-                    </li>
-                    <li>
-                        <UISrefActive class="active">
-                            <UISref to="hittaHit">
-                                <a>Hitta hit</a>
-                            </UISref>
-                        </UISrefActive>
-                    </li>
-                    <li>
-                        <UISrefActive class="active">
-                            <UISref to="tidProgram">
-                                <a>Tid och program</a>
-                            </UISref>
-                        </UISrefActive>
-                    </li>
-                    <li>
-                        <UISrefActive class="active">
-                            <UISref to="kladkod">
-                                <a>Klädkod</a>
-                            </UISref>
-                        </UISrefActive>
-                    </li>
-                    <li>
-                        <UISrefActive class="active">
-                            <UISref to="overnattning">
-                                <a>Övernattning</a>
-                            </UISref>
-                        </UISrefActive>
-                    </li>
-                    <li>
-                        <UISrefActive class="active">
-                            <UISref to="onskelista">
-                                <a>Önskelista</a>
-                            </UISref>
-                        </UISrefActive>
-                    </li>
-                    <li>
-                        <UISrefActive class="active">
-                            <UISref to="toastmaster">
-                                <a>Toastmaster</a>
-                            </UISref>
-                        </UISrefActive>
-                    </li>
-                    <li>
-                        <UISrefActive class="active">
-                            <UISref to="praktiskInfo">
-                                <a>Kontaktuppgifter</a>
-                            </UISref>
-                        </UISrefActive>
-                    </li>
-                    {
-                        this.isAdmin() ? (
-                            <li>
-                                <UISrefActive class="active">
-                                    <UISref to="admin">
-                                        <a>Admin</a>
-                                    </UISref>
-                                </UISrefActive>
-                            </li>
-                        ) : null
-                    }
-                </ul>
-            </div>
+            <>
+                <div onClick={this.toggleMenu} className={`mobile-header ${this.state.isOpen ? 'open' : ''}`}>
+                    <i className="fa fa-bars menu-icon"></i>
+                    <span className="current-state">{ this.state.currentState }</span>
+                </div>
+                <div className={`nav-wrapper ${this.state.isOpen ? 'open' : ''}`}>
+                    <ul>
+                        <li>
+                            <UISrefActive class="active">
+                                <UISref to="rsvp">
+                                    <a onClick={this.toggleMenu}>OSA</a>
+                                </UISref>
+                            </UISrefActive>
+                        </li>
+                        <li>
+                            <UISrefActive class="active">
+                                <UISref to="hittaHit">
+                                    <a onClick={this.toggleMenu}>Hitta hit</a>
+                                </UISref>
+                            </UISrefActive>
+                        </li>
+                        <li>
+                            <UISrefActive class="active">
+                                <UISref to="tidProgram">
+                                    <a onClick={this.toggleMenu}>Tid och program</a>
+                                </UISref>
+                            </UISrefActive>
+                        </li>
+                        <li>
+                            <UISrefActive class="active">
+                                <UISref to="kladkod">
+                                    <a onClick={this.toggleMenu}>Klädkod</a>
+                                </UISref>
+                            </UISrefActive>
+                        </li>
+                        <li>
+                            <UISrefActive class="active">
+                                <UISref to="overnattning">
+                                    <a onClick={this.toggleMenu}>Övernattning</a>
+                                </UISref>
+                            </UISrefActive>
+                        </li>
+                        <li>
+                            <UISrefActive class="active">
+                                <UISref to="onskelista">
+                                    <a onClick={this.toggleMenu}>Önskelista</a>
+                                </UISref>
+                            </UISrefActive>
+                        </li>
+                        <li>
+                            <UISrefActive class="active">
+                                <UISref to="toastmaster">
+                                    <a onClick={this.toggleMenu}>Toastmaster</a>
+                                </UISref>
+                            </UISrefActive>
+                        </li>
+                        <li>
+                            <UISrefActive class="active">
+                                <UISref to="praktiskInfo">
+                                    <a onClick={this.toggleMenu}>Kontaktuppgifter</a>
+                                </UISref>
+                            </UISrefActive>
+                        </li>
+                        {
+                            this.isAdmin() ? (
+                                <li>
+                                    <UISrefActive class="active">
+                                        <UISref to="admin">
+                                            <a onClick={this.toggleMenu}>Admin</a>
+                                        </UISref>
+                                    </UISrefActive>
+                                </li>
+                            ) : null
+                        }
+                    </ul>
+                </div>
+            </>
         )
     }
 }
