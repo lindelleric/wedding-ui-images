99ba66d838e2c1fd6e047fc56e61c0c8365442ba
Eric Nilsson <eric.nilsson@hotmail.com>
2019-07-16 22:48:57 +0200

    fix(videos): attempt to set the first frame as the thumbnail for mobile

--- src/components/Filmer/Filmer.tsx
+++ src/components/Filmer/Filmer.tsx
@@ -5,15 +5,15 @@ import './Filmer.less';
 export class Filmer extends React.Component {
     public render = () => (
         <div className="film-wrapper">
-            <video src={`/static/videos/vigsel.mp4?jwt=${localStorage.getItem('token')}`} controls></video>
-            <video src={`/static/videos/leif.mp4?jwt=${localStorage.getItem('token')}`} controls></video>
-            <video src={`/static/videos/brodersang.mov?jwt=${localStorage.getItem('token')}`} controls></video>
+            <video src={`/static/videos/vigsel.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} controls preload="metadata"></video>
+            <video src={`/static/videos/leif.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} controls preload="metadata"></video>
+            <video src={`/static/videos/brodersang.mov?jwt=${localStorage.getItem('token')}#t=0.5`} controls preload="metadata"></video>
 
-            <video src={`/static/videos/bruddans.mp4?jwt=${localStorage.getItem('token')}`} height="600" width="auto" controls></video>
-            <video src={`/static/videos/dans1.mp4?jwt=${localStorage.getItem('token')}`} height="600" width="auto" controls></video>
-            <video src={`/static/videos/dans2.mp4?jwt=${localStorage.getItem('token')}`} height="600" width="auto" controls></video>
-            <video src={`/static/videos/dans3.mp4?jwt=${localStorage.getItem('token')}`} height="600" width="auto" controls></video>
-            {/*<video src={`/static/videos/hanna.mp4?jwt=${localStorage.getItem('token')}`} controls></video>*/}
+            <video src={`/static/videos/bruddans.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} height="600" width="auto" controls preload="metadata"></video>
+            <video src={`/static/videos/dans1.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} height="600" width="auto" controls preload="metadata"></video>
+            <video src={`/static/videos/dans2.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} height="600" width="auto" controls preload="metadata"></video>
+            <video src={`/static/videos/dans3.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} height="600" width="auto" controls preload="metadata"></video>
+            {/*<video src={`/static/videos/hanna.mp4?jwt=${localStorage.getItem('token')}`} controls preload="metadata"></video>*/}
         </div>
     );
 }
