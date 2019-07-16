5a7985643aff4defa4a696666a42ea65f21cda23
Eric Nilsson <eric.nilsson@hotmail.com>
2019-07-17 00:06:37 +0200

    feat(videos): attempt to add thumbnail to mobile

--- src/components/Filmer/Filmer.tsx
+++ src/components/Filmer/Filmer.tsx
@@ -5,14 +5,14 @@ import './Filmer.less';
 export class Filmer extends React.Component {
     public render = () => (
         <div className="film-wrapper">
-            <video src={`/static/videos/vigsel.mp4?jwt=${localStorage.getItem('token')}`} controls></video>
-            <video src={`/static/videos/leif.mp4?jwt=${localStorage.getItem('token')}`} controls></video>
-            <video src={`/static/videos/brodersang.mov?jwt=${localStorage.getItem('token')}`} controls></video>
+            <video src={`/static/videos/vigsel.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} controls></video>
+            <video src={`/static/videos/leif.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} controls></video>
+            <video src={`/static/videos/brodersang.mov?jwt=${localStorage.getItem('token')}#t=0.5`} controls></video>
 
-            <video src={`/static/videos/bruddans.mp4?jwt=${localStorage.getItem('token')}`} height="600" width="auto" controls></video>
-            <video src={`/static/videos/dans1.mp4?jwt=${localStorage.getItem('token')}`} height="600" width="auto" controls></video>
-            <video src={`/static/videos/dans2.mp4?jwt=${localStorage.getItem('token')}`} height="600" width="auto" controls></video>
-            <video src={`/static/videos/dans3.mp4?jwt=${localStorage.getItem('token')}`} height="600" width="auto" controls></video>
+            <video src={`/static/videos/bruddans.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} height="600" width="auto" controls></video>
+            <video src={`/static/videos/dans1.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} height="600" width="auto" controls></video>
+            <video src={`/static/videos/dans2.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} height="600" width="auto" controls></video>
+            <video src={`/static/videos/dans3.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} height="600" width="auto" controls></video>
             {/*<video src={`/static/videos/hanna.mp4?jwt=${localStorage.getItem('token')}`} controls></video>*/}
         </div>
     );
