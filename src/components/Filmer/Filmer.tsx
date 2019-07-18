b64194775f988d1909e587e958e8b44238d05030
Eric Nilsson <eric.nilsson@hotmail.com>
2019-07-18 22:12:29 +0200

    feat(video): add posters to videos

--- src/components/Filmer/Filmer.tsx
+++ src/components/Filmer/Filmer.tsx
@@ -3,17 +3,18 @@ import React from 'react';
 import './Filmer.less';
 
 export class Filmer extends React.Component {
-    public render = () => (
-        <div className="film-wrapper">
-            <video src={`/static/videos/vigsel.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} controls></video>
-            <video src={`/static/videos/leif.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} controls></video>
-            <video src={`/static/videos/brodersang.mov?jwt=${localStorage.getItem('token')}#t=0.5`} controls></video>
+    public render() {
+        const token = localStorage.getItem('token');
+        return (
+            <div className="film-wrapper">
+                <video src={`/static/videos/vigsel.mp4?jwt=${token}`} className="blocked" controls poster={`/static/video-thumbs/vigsel.png?jwt=${token}`} preload="none"></video>
+                <video src={`/static/videos/leif.mp4?jwt=${token}`} className="blocked" controls poster={`/static/video-thumbs/leif.png?jwt=${token}`} preload="none"></video>
+                <video src={`/static/videos/brodersang.mov?jwt=${token}`} className="blocked" controls poster={`/static/video-thumbs/brodersang.png?jwt=${token}`} preload="none"></video>
 
-            <video src={`/static/videos/bruddans.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} height="600" width="auto" controls></video>
-            <video src={`/static/videos/dans1.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} height="600" width="auto" controls></video>
-            <video src={`/static/videos/dans2.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} height="600" width="auto" controls></video>
-            <video src={`/static/videos/dans3.mp4?jwt=${localStorage.getItem('token')}#t=0.5`} height="600" width="auto" controls></video>
-            {/*<video src={`/static/videos/hanna.mp4?jwt=${localStorage.getItem('token')}`} controls></video>*/}
-        </div>
-    );
+                <video src={`/static/videos/bruddans.mp4?jwt=${token}`} height="600" width="auto" controls poster={`/static/video-thumbs/bruddans.png?jwt=${token}`} preload="none"></video>
+                <video src={`/static/videos/dans1.mp4?jwt=${token}`} height="600" width="auto" controls poster={`/static/video-thumbs/dans1.png?jwt=${token}`} preload="none"></video>
+                <video src={`/static/videos/dans2.mp4?jwt=${token}`} height="600" width="auto" controls poster={`/static/video-thumbs/dans2.png?jwt=${token}`} preload="none"></video>
+            </div>
+        );
+    }
 }
